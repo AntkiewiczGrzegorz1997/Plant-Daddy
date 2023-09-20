2c7a39d75d0cb35763bb0bd929748eba0d7192e3
AntkiewiczGrzegorz1997 <gantkiewicz97@gmail.com>
2023-09-20 20:45:06 +0200

    adding all files from today

---
+++ database/dbScripts.sql
@@ -0,0 +1,88 @@
+/*Create a DB*/
+
+CREATE DATABASE plantasia;
+
+
+/*connect to the Database*/
+
+\c plantasia
+
+
+/*create tables*/
+
+CREATE TABLE Users (
+  user_ID bigint PRIMARY KEY,
+  email varchar(80),
+  password varchar(200),
+  firstName varchar(80),
+  lastName varchar(80)
+);
+
+/*  FOREIGN KEY (user_ID) REFERENCES User_Plants(user_ID)*/
+
+
+CREATE TABLE User_Plants (
+  user_ID bigint,
+  plant_ID bigint,
+  ID varchar(10),
+  plant_name varchar(200), 
+  full_name varchar(200),
+  scientific_name varchar(200), 
+  plant_description varchar(1000),
+  plant_size varchar(80), 
+  age int, 
+  watering varchar(20),
+  sunlight varchar(200),
+  icon_ID int,
+  PRIMARY KEY (user_ID, plant_ID),
+ 
+
+  
+);
+
+/*   FOREIGN KEY (user_ID) REFERENCES Users(user_ID),
+  FOREIGN KEY (user_ID, plant_ID) REFERENCES Uploaded_Images(user_ID, plant_ID),
+  FOREIGN KEY (ID) REFERENCES Plants(ID)
+  */
+
+
+
+
+CREATE TABLE Plants (
+  ID varchar(10) PRIMARY KEY,
+  full_name varchar(200),
+  scientific_name varchar(200), 
+  other_name varchar(200),
+  image_url varchar(500),
+  sunlight varchar(200),
+  
+);
+
+
+/*   FOREIGN KEY (ID) REFERENCES User_Plants(ID)
+  */
+
+
+
+
+CREATE TABLE Uploaded_Images (
+  user_ID bigint,
+  plant_ID bigint,
+  image_ID bigint,
+  image_url varchar(500),
+  PRIMARY KEY (user_ID, plant_ID, image_ID),
+  
+
+  
+);
+
+/*
+FOREIGN KEY (user_ID, plant_ID) REFERENCES User_Plants(user_ID, plant_ID)
+ */ 
+
+
+ 
+
+
+/*create tables*/
+
---
+++ scraper/scraper.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const apiUrl = 'https://perenual.com/api/species-list';
+const apiKey = 'sk-h1y9650afd30ac0db2194'; // Replace with your actual API key
+const perPage = 30; // Number of items per page
+let currentPage = 1;
+let data = [];
+
+const fetchData = async (page) => {
+  try {
+    const response = await axios.get(apiUrl, {
+      params: {
+        key: apiKey,
+        page,
+      },
+    });
+
+    if (response.status !== 200) {
+      console.error(`Failed to fetch page ${page}. Exiting.`);
+      return null;
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error(`Error while fetching page ${page}:`, error.message);
+    return null;
+  }
+};
+
+const fetchAllData = async () => {
+  while (true) {
+    const pageData = await fetchData(currentPage);
+
+    if (!pageData) {
+      break;
+    }
+
+    data = data.concat(pageData.data);
+
+    if (currentPage >= pageData.last_page) {
+      break;
+    }
+
+    currentPage++;
+  }
+};
+
+const saveDataToJsonFile = () => {
+  const jsonData = {
+    data: data,
+  };
+
+  fs.writeFileSync('combined_data.json', JSON.stringify(jsonData, null, 2));
+  console.log('Data retrieval completed and saved to combined_data.json.');
+};
+
+fetchAllData().then(() => {
+  saveDataToJsonFile();
+});
