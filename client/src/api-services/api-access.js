8be5c19d9c19367b30e64fb2059838afb9b1ceb8
Grzegorz Antkiewicz <grzegorzantkiewicz@MacBook-Pro-5.local>
2023-11-01 19:26:36 +0100

    add file

---
+++ client/src/api-services/api-access.js
@@ -0,0 +1,11 @@
+export const apis = {
+  perenual: {
+    apiUrl: 'https://perenual.com',
+    apiKey: 'sk-h1y9650afd30ac0db2194',
+  },
+
+  cloudinary: {
+    uploadPreset: 'bspqor5ddd',
+    apiKey: '888174412151925',
+  },
+};
